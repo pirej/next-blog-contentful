a0d4da9db80478c1670d02a810ebfb19c0c66768
Igor Risteski <pirej30@yahoo.com>
2021-07-10 04:01:37 +0200

    initial cleanup and add base layout

---
+++ components/Layout.js
@@ -0,0 +1,46 @@
+import Link from "next/link";
+import Image from "next/image";
+import logoimg from "../public/logo.svg";
+
+export default function Layout({ children }) {
+  let date = new Date().getFullYear();
+  return (
+    <div className="layout">
+      <nav className="navbar">
+        <div>
+          <Link href="/" passHref>
+            <Image src={logoimg} alt="logo" className="logo" />
+          </Link>
+        </div>
+        <div>
+          <Link href="#">
+            <a className="link">link1</a>
+          </Link>
+          <Link href="#">
+            <a className="link">link2</a>
+          </Link>
+          <Link href="#">
+            <a className="link">link3</a>
+          </Link>
+        </div>
+      </nav>
+      <header>
+        <Link href="/">
+          <a>
+            <h1>
+              <span>Trying to Learn</span>
+              <span>Web Development</span>
+            </h1>
+            <h2>New tricks for Old dogs</h2>
+          </a>
+        </Link>
+      </header>
+
+      <div className="page-content">{children}</div>
+
+      <footer>
+        <p>&copy; WebDev.Club {date}</p>
+      </footer>
+    </div>
+  );
+}
