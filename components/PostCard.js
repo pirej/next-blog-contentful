9636c2a42af435c371c8760ff7e0f1124366ed02
Igor Risteski <pirej30@yahoo.com>
2021-07-12 03:56:43 +0200

    Render the post title, thumbnails and links to read more

--- components/PostCard.js
+++ components/PostCard.js
@@ -1,5 +1,30 @@
+import Image from "next/image";
+import Link from "next/link";
+
 function PostCard({ post }) {
-  return <div>{post.fields.title}</div>;
+  const { title, slug, featuredImage, thumbnail } = post.fields;
+  return (
+    <div className="card">
+      <div className="featured">
+        <Image
+          src={`https:${thumbnail.fields.file.url}`}
+          width={thumbnail.fields.file.details.image.width}
+          height={thumbnail.fields.file.details.image.height}
+          alt={featuredImage.fields.file.title}
+        />
+      </div>
+      <div className="content">
+        <div className="info">
+          <h4>{title}</h4>
+        </div>
+        <div className="actions">
+          <Link href={`/posts/${slug}`}>
+            <a>Read more</a>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default PostCard;
