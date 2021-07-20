4f88d5d6d469ea9a08114c495c7ba20701fc7127
Igor Risteski <pirej30@yahoo.com>
2021-07-20 02:52:29 +0200

    Initial Algolia search

---
+++ scripts/build-search.js
@@ -0,0 +1,12 @@
+const dotenv = require("dotenv");
+
+async function getAllBlogPosts() {
+  // write your code to fetch your data
+}
+
+(async function () {
+  // initialize environment variables
+  dotenv.config();
+
+  console.log("Let's fetch some data!");
+})();
