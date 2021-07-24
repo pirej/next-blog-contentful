92521cc429e8ed6d2481d3a5ce11cf9909aaa2f0
Igor Risteski <pirej30@yahoo.com>
2021-07-24 02:36:59 +0200

    Discipline the search from.. it was missbehaving :)

--- components/Search/CustomHits.js
+++ components/Search/CustomHits.js
@@ -1,9 +1,19 @@
+import { useState, useEffect } from "react";
 import Link from "next/link";
 import { connectStateResults } from "react-instantsearch-dom";
 import styles from "../../styles/InstantSearch.module.scss";
 
 function Hits({ searchState, searchResults }) {
+  const [showResults, setShowResults] = useState(true);
   const validQuery = searchState.query?.length >= 3;
+
+  function OnclickHandler(e) {
+    setShowResults(!showResults);
+  }
+
+  useEffect(() => {
+    validQuery && setShowResults(true);
+  }, [validQuery]);
 
   return (
     <div className={styles.instantSearch__hits}>
@@ -12,19 +22,23 @@ function Hits({ searchState, searchResults }) {
           Aw snap! No search results were found.
         </p>
       )}
-      {searchResults?.hits.length > 0 && validQuery && (
-        <ol className={styles.instantSearch__hitsList}>
-          {searchResults.hits.map(hit => (
-            <li
-              key={hit.objectID}
-              className={styles.instantSearch__hitsListItem}
-            >
-              <Link href={`/posts/${hit.slug}`}>
-                <a>{hit.title}</a>
-              </Link>
-            </li>
-          ))}
-        </ol>
+      {showResults && (
+        <>
+          {searchResults?.hits.length > 0 && validQuery && (
+            <ol className={styles.instantSearch__hitsList}>
+              {searchResults.hits.map(hit => (
+                <li
+                  key={hit.objectID}
+                  className={styles.instantSearch__hitsListItem}
+                >
+                  <Link href={`/posts/${hit.slug}`}>
+                    <a onClick={e => OnclickHandler(e)}>{hit.title}</a>
+                  </Link>
+                </li>
+              ))}
+            </ol>
+          )}
+        </>
       )}
     </div>
   );
--- components/Search/CustomSearchBox.js
+++ components/Search/CustomSearchBox.js
@@ -1,19 +1,47 @@
+import { useEffect, useRef, useState } from "react";
 import { connectSearchBox } from "react-instantsearch-dom";
 import { FaSearch } from "react-icons/fa";
 import styles from "../../styles/InstantSearch.module.scss";
 
 function SearchBox({ refine }) {
+  const ref = useRef();
+  const [newSearch, setNewSearch] = useState("");
+
+  useEffect(e => {
+    const checkIfClickedOutside = e => {
+      // clear If the clicked target is not within the form,
+      if (ref.current && !ref.current.contains(e.target)) {
+        setNewSearch("");
+      }
+    };
+    document.addEventListener("mousedown", checkIfClickedOutside);
+    return () => {
+      // Cleanup the event listener
+      document.removeEventListener("mousedown", checkIfClickedOutside);
+    };
+  }, []);
+
   return (
-    <form action="" role="search" className={styles.instantSearch__form}>
+    <form
+      action=""
+      role="search"
+      autoComplete="off"
+      className={styles.instantSearch__form}
+    >
       <label htmlFor="algolia_search" className={styles.instantSearch__label}>
         <FaSearch />
       </label>
       <input
+        ref={ref}
         className={styles.instantSearch__input}
         id="algolia_search"
         type="search"
+        value={newSearch}
         placeholder="search.."
-        onChange={e => refine(e.currentTarget.value)}
+        onChange={e => {
+          refine(e.currentTarget.value);
+          setNewSearch(e.currentTarget.value);
+        }}
       />
     </form>
   );
